58b69629f951a6132701feb43b579be98a3de83a
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2020-12-27 16:26:52 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,8 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+client.once('ready', () => {
+    console.log('harrybot is online');
+});
 
-client.login('NzkyOTAxMTI3MTExNzA0NjA3.X-kcuA.orHsfZek-9SmYf_ZmUaCrec4hDU')
+client.login('NzkyOTAxMTI3MTExNzA0NjA3.X-kcuA.kxjVLsaml7MYXoBRDijC7Apk_IY')
