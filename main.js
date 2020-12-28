c9e05d8e80709c554e717b29c09d62220ddbbede
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2020-12-27 16:35:40 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,18 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const prefix = '/';
+
 client.once('ready', () => {
     console.log('harrybot is online');
 });
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    const args = message.content.slice(prefix.length).split(/ +/);
+    
+
+})
 
 client.login('NzkyOTAxMTI3MTExNzA0NjA3.X-kcuA.kxjVLsaml7MYXoBRDijC7Apk_IY')
